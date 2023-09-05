e598ed324dcdffe88307661d381fa0fbce71bd5a
DuyLP <tandv@unit.com.vn>
2023-09-06 00:38:49 +0700

    [DuyLP]

--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -147,7 +147,7 @@ export default {
       const url = window.URL.createObjectURL(blob);
       const link = document.createElement('a');
       link.href = url;
-      link.setAttribute('download', 'CongViec-Upload-Report.xlsx');
+      link.setAttribute('download', this.table + '.xlsx');
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
